787a2022b2f96941223364440d5c9bbd1a073e9a
wasamisam0119 <zhaoshujia0123@gmail.com>
2016-12-07 22:39:01 +0000

    add Tuple class for saving Item

---
+++ DiamondHunter/src/Mapviewer/Tuple.java
@@ -0,0 +1,20 @@
+package Mapviewer;
+
+/**
+ * Created by Sam_Du on 16/12/7.
+ */
+public class Tuple {
+    public int x;
+    public int y;
+
+    public Tuple(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void setPosition(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+
+}
