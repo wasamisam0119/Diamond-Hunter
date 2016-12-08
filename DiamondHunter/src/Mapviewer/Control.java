f43a7ecfbd31465c0712e70cc6e7763427584156
wasamisam0119 <zhaoshujia0123@gmail.com>
2016-12-08 16:28:05 +0000

    add more Exception Handler and comments

--- DiamondHunter/src/Mapviewer/Control.java
+++ DiamondHunter/src/Mapviewer/Control.java
@@ -15,137 +15,163 @@ import javafx.scene.control.Alert;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Control {
-    int x,y;
+    int x, y;
     public static final int BOAT = 0;
     public static final int AXE = 1;
     public static final int TILESIZE = 16;
-    public int [][] mapValue;   // the value of each of the map block
+    public int[][] mapValue;   // the value of each of the map block
+    public Image map;
+    public Image image;
     private GameMap gameMap;
     private GraphicsContext gContext;
     private ObjectInputStream objectReader;
     private ObjectOutputStream objectWriter;
-    public Image map=new Image(getClass().getResourceAsStream("/Tilesets/testtileset.gif"));
-    public Image image=new Image(getClass().getResourceAsStream("/Sprites/items.gif"));
-    String filename= "ItemMap.data";
+    String filename = "ItemMap.data";
     File file;
     Tuple axe = null;   //store the item's positon
     Tuple boat = null;
-    HashMap<Integer,Tuple> items;
-    @FXML private TextArea xPosition,yPosition;
-    @FXML private Canvas canvas;
+    HashMap<Integer, Tuple> items;
+    @FXML
+    private TextArea xPosition, yPosition;
+    @FXML
+    private Canvas canvas;
+
+
     public void initialize() {
-
-        try {
-            gameMap = new GameMap(TILESIZE, TILESIZE, map);
-            gameMap.loadMap("/Maps/testmap.map");
-            gContext = canvas.getGraphicsContext2D();
-            gameMap.drawMap(gContext);
-            file= new File(filename);
-            if (!file.exists())
-            {
-                items= new HashMap<Integer,Tuple>();
-            }
-            else{
-                objectReader = new ObjectInputStream(new FileInputStream(file));
-                items=(HashMap<Integer,Tuple>) objectReader.readObject();
-                objectReader.close();
-
-            }
-            if (items.containsKey(0))
-            {
-                boat=items.get(0);
-                drawItem(0,boat.x,boat.y);
-            }
-            if (items.containsKey(1))
-            {
-                axe=items.get(1);
-                drawItem(1,axe.x,axe.y);
-            }
-            // initialize game map
-        }catch (IOException e){
-            e.printStackTrace();
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+        //initialize the picture resource
+        map = new Image(getClass().getResourceAsStream("/Tilesets/testtileset.gif"));
+        image = new Image(getClass().getResourceAsStream("/Sprites/items.gif"));
+        gameMap = new GameMap(TILESIZE, TILESIZE, map);
+        gameMap.loadMap("/Maps/testmap.map");
+        gContext = canvas.getGraphicsContext2D();
+        //draw the map
+        gameMap.drawMap(gContext);
+        file = new File(filename);
+        //load the item file
+        if (!file.exists()) {
+            items = new HashMap<Integer, Tuple>();
         }
+        else {
+            try {
+                objectReader = new ObjectInputStream(new FileInputStream(file));
+                items = (HashMap<Integer, Tuple>) objectReader.readObject();
+                objectReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
 
-
+        }
+        if (items.containsKey(0)) {
+            boat = items.get(0);
+            drawItem(0, boat.x, boat.y);
+        }
+        if (items.containsKey(1)) {
+            axe = items.get(1);
+            drawItem(1, axe.x, axe.y);
+        }
     }
 
-    @FXML public void axePressed() {
+    /* this method is used to handle the button action*/
+    @FXML
+    public void axePressed() {
         canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 System.out.println("X: " + event.getX() + " Y: " + event.getY());
-                x = (int)event.getX()/16;
-                y = (int)event.getY()/16;
-                System.out.println(y);
-                mapValue=gameMap.getMap();
-                /*for (int i=0;i<40;i++)
-                {
-                    for (int j=0;j<40;j++)
-                    {
-                        System.out.print(mapValue[i][j]);
-                        System.out.print(" ");
-                    }
-                    System.out.println();
-                }*/
-                int currentPoint=mapValue[y][x];
-                checkAvailable(currentPoint,x,y);
+                x = (int) event.getX() / 16;
+                y = (int) event.getY() / 16;
+                mapValue = gameMap.getMap();
+                int currentPoint = mapValue[y][x];
+                checkAvailable(currentPoint, x, y, AXE);
 
             }
         });
 
     }
 
-    @FXML public void boatPressed() {
-        //setPosition();
+    @FXML
+    public void boatPressed() {
+        canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                x = (int) event.getX() / 16;
+                y = (int) event.getY() / 16;
+                mapValue = gameMap.getMap();
+                int currentPoint = mapValue[y][x];
+                checkAvailable(currentPoint, x, y, BOAT);
+
+            }
+        });
+
     }
 
-    public void checkAvailable (int value,int x, int y) {
+    /*this method is to check if a position is available to set item,
+     *if available, draw the item on canvas and put it into a tuple object
+     */
+    public void checkAvailable(int value, int x, int y, int type) {
 
-        if (value == 20 || value == 22 || value == 21) {
+        if (value == 20 || value == 22 || value == 21) {        //item can not set on the barrier
 
-            ringAlert(Alert.AlertType.ERROR,"This positon is not available!");
-        }
-        else if (axe != null) {
-            drawItem(3, axe.x, axe.y);
-            axe.setPosition(x, y);
-            displayCoordinate();
-            drawItem(1, x, y);
+            ringAlert(Alert.AlertType.ERROR, "This positon is not available!");
         }
         else {
 
-            drawItem(1, x, y);
-            axe = new Tuple(x,y);
-            displayCoordinate();
+            if (type == AXE) {
+                if (axe != null) {     //if already have axe, change the positon and draw the axe again
+                    drawItem(3, axe.x, axe.y);
+                    axe.setPosition(x, y);
+                    displayCoordinate();
+                    drawItem(1, x, y);
+                }
+                else {                  //if do not have item, add one to the axe object and draw
+                    drawItem(1, x, y);
+                    axe = new Tuple(x, y);
+                    displayCoordinate();
+                }
+            }
+            else {
+                if (boat != null) {     //if already have boat, change the positon and draw the axe again
+                    drawItem(3, boat.x, boat.y);
+                    boat.setPosition(x, y);
+                    displayCoordinate();
+                    drawItem(0, x, y);
+                }
+                else {                  //if do not have item, add one to the boat object and draw
+                    drawItem(0, x, y);
+                    boat = new Tuple(x, y);
+                    displayCoordinate();
+                }
+            }
         }
     }
 
-    public void displayCoordinate()
+    public void displayCoordinate() //display the item position on TextArea after set
     {
         xPosition.setText(Integer.toString(x));
         yPosition.setText(Integer.toString(y));
     }
-    public void ringAlert(Alert.AlertType alertType, String message){
+
+    public void ringAlert(Alert.AlertType alertType, String message) {
         Alert alert = new Alert(alertType, message);
         alert.showAndWait();
     }
-    @FXML public void saveClose(){
-        if (boat!=null)
-        {
-            items.put(0,boat);
+
+    @FXML
+    public void saveClose() {
+        if (boat != null) {
+            items.put(0, boat);
         }
-        if (axe!=null)
-        {
-            items.put(1,axe);
+        if (axe != null) {
+            items.put(1, axe);
         }
-        if (!items.isEmpty())
-        {
+        if (!items.isEmpty()) {
             try {
                 file.createNewFile();
-            }catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             objectWriter(file);
@@ -153,21 +179,18 @@ public class Control {
         System.exit(0);
     }
 
-    public void drawItem(int type, int x, int y ){
-        PixelReader pixelReader= image.getPixelReader();
+    public void drawItem(int type, int x, int y) {
+        PixelReader pixelReader = image.getPixelReader();
         PixelReader p = map.getPixelReader();
 
-        if (type==1)
-        {
-            canvas.getGraphicsContext2D().drawImage(new WritableImage(pixelReader, TILESIZE, TILESIZE, TILESIZE, TILESIZE), x * TILESIZE , y * TILESIZE);
+        if (type == 1) {
+            canvas.getGraphicsContext2D().drawImage(new WritableImage(pixelReader, TILESIZE, TILESIZE, TILESIZE, TILESIZE), x * TILESIZE, y * TILESIZE);
         }
-        else if (type==0)
-        {
-            canvas.getGraphicsContext2D().drawImage(new WritableImage(pixelReader,0, TILESIZE, TILESIZE, TILESIZE), x * TILESIZE , y * TILESIZE);
-
+        else if (type == 0) {
+            canvas.getGraphicsContext2D().drawImage(new WritableImage(pixelReader, 0, TILESIZE, TILESIZE, TILESIZE), x * TILESIZE, y * TILESIZE);
         }
-        else{
-            canvas.getGraphicsContext2D().drawImage(new WritableImage(p,TILESIZE,0,TILESIZE,TILESIZE),x*TILESIZE,y*TILESIZE);
+        else {
+            canvas.getGraphicsContext2D().drawImage(new WritableImage(p, TILESIZE, 0, TILESIZE, TILESIZE), x * TILESIZE, y * TILESIZE);
         }
     }
 
@@ -178,6 +201,7 @@ public class Control {
             objectWriter.close();
         } catch (IOException ioe) {
             System.err.println("cannot write " + filename + " for writing!");
+            ioe.printStackTrace();
         }
 
     }
