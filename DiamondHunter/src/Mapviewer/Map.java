e0478f8850754a78dc58f98322ff66522d86e16e
wasamisam0119 <zhaoshujia0123@gmail.com>
2016-12-08 15:17:05 +0000

    delete unused method

--- DiamondHunter/src/Mapviewer/Map.java
+++ DiamondHunter/src/Mapviewer/Map.java
@@ -6,26 +6,23 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.io.*;
+
 
 public class Map extends Application {
-        @Override
-        public void start(Stage primaryStage) {
-            try {
-                BorderPane root = new BorderPane();
-                Scene scene = new Scene(root,640,720);
-                MainCanvas mainCanvas = new MainCanvas(640, 720);
-                root.getChildren().add(mainCanvas);
-                Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("Mapviewer/editor.fxml"));
-                //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-                root.setCenter(content);
-                primaryStage.setScene(scene);
-                primaryStage.show();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public static void main(String[] args) {
-            launch(args);
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("Mapviewer/editor.fxml"));
+            Scene scene = new Scene(content, 640, 760);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
